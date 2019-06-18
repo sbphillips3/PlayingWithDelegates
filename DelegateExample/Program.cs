dfc3b2f1a0592a6210b8fa0f37d3a4f11e9af434
Sean Phillips <36488351+sbphillips3@users.noreply.github.com>
2019-06-17 19:57:46 -0500

    Add files via upload
    C# Delegate example from a book I'm reading. A delegate is used to call two separate methods in a delegate array, iterating through three double numbers and displaying them.

---
+++ DelegateExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/** Playing with Delegates */
+// Sean Phillips 6/17/2019
+
+namespace DelegateExample
+{
+
+    class MathOperations
+    {
+        public static double MultiplyByTwo(double value) => value * 2;
+        public static double Square(double value) => value * value;
+    }
+
+    delegate double DoubleOp(double x);
+    class Program
+    {
+        static void Main()
+        {
+            DoubleOp[] operations =
+            {
+                MathOperations.MultiplyByTwo,
+                MathOperations.Square
+            };
+
+            for( int i = 0; i < operations.Length; i++)
+            {
+                Console.WriteLine($"Using operations[{i}] :");
+                ProcessAndDisplayNumber(operations[i], 2.0);
+                ProcessAndDisplayNumber(operations[i], 7.94);
+                ProcessAndDisplayNumber(operations[i], 1.414);
+                Console.WriteLine();
+                
+            }
+            Console.ReadLine();
+        }
+
+        static void ProcessAndDisplayNumber(DoubleOp action, double value)
+        {
+            double result = action(value);
+            Console.WriteLine($"Value is {value}, result of operation is {result}");
+        }
+    }
+}
